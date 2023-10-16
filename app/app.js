7a65e0981a63d9c2ccd11d6b0392d35c6f62627e
aberkayk <aberkayk@gmail.com>
2023-10-16 16:57:09 +0300

    Modify paramter name

--- app/app.js
+++ app/app.js
@@ -1,54 +1,54 @@
 import { getQuestion } from "../services/questions.js";
 import { getProducts } from "../services/products.js";
 import {
-	getStorage,
-	updateStorage,
-	stepCounter,
+  getStorage,
+  updateStorage,
+  stepCounter,
 } from "../utils/localStorage.js";
 
 const form = document.getElementById("form");
 const content = form.querySelector(".content");
 
 export const showLoading = () => {
-	content.innerHTML = `<div class="loading">Filtering Products</div>`;
+  content.innerHTML = `<div class="loading">Filtering Products</div>`;
 };
 
 export const notFound = () => {
-	content.innerHTML = `
+  content.innerHTML = `
     <div class="not-found">
       <p>Products Not Found</p>
       <button id="backButton">Back</button>
     </div>
   `;
 
-	const backButton = document.getElementById("backButton");
+  const backButton = document.getElementById("backButton");
 
-	backButton.addEventListener("click", (event) => {
-		event.preventDefault();
-		updateStorage(2);
-	});
+  backButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    updateStorage(2);
+  });
 };
 
 export const showFilteredProducts = (products) => {
-	const productHTML = products
-		.map((product) => {
-			return `
+  const productHTML = products
+    .map((product) => {
+      return `
       <div class="swiper-slide">
         <div class="slider-image-wrapper"><img src=${
-					product.image
-				} loading="lazy" /></div>
+          product.image
+        } loading="lazy" /></div>
         <p class="product-title">${product.name}</p>
         <p class="product-price">€${product.price}</p>
         <p class="product-old-price">${
-					product.oldPrice ? "€" + product.oldPrice : ""
-				}</p>
+          product.oldPrice ? "€" + product.oldPrice : ""
+        }</p>
         <button disabled>VIEW PRODUCT</button>
       </div>
     `;
-		})
-		.join("");
+    })
+    .join("");
 
-	content.innerHTML = `
+  content.innerHTML = `
     <div class="product-container">
       <div class="swiper mySwiper">
         <div class="swiper-wrapper">
@@ -64,62 +64,59 @@ export const showFilteredProducts = (products) => {
     </div>
   `;
 
-	new Swiper(".mySwiper", {
-		slidesPerView: 1,
-		spaceBetween: 30,
-		loop: true,
-		pagination: {
-			el: ".swiper-pagination",
-			clickable: true,
-		},
-		navigation: {
-			nextEl: ".swiper-button-next",
-			prevEl: ".swiper-button-prev",
-		},
-	});
+  new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 
-	if (products.length >= 15) {
-		document.querySelectorAll(".swiper-pagination-bullet").forEach((el) => {
-			el.style.cssText = "width:1.5%";
-		});
-	}
+  if (products.length >= 15) {
+    document.querySelectorAll(".swiper-pagination-bullet").forEach((el) => {
+      el.style.cssText = "width:1.5%";
+    });
+  }
 
-	const resetButton = document.getElementById("resetButton");
+  const resetButton = document.getElementById("resetButton");
 
-	resetButton.addEventListener("click", (event) => {
-		event.preventDefault();
-		updateStorage(0);
-	});
+  resetButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    updateStorage(0);
+  });
 };
 
 export const showQuestion = async (currentStep) => {
-	const [question, totalNumberOfSteps] = await Promise.all([
-		getQuestion(currentStep),
-		stepCounter(getStorage()),
-	]);
+  const [question, stepIndex] = await Promise.all([
+    getQuestion(currentStep),
+    stepCounter(getStorage()),
+  ]);
 
-	const buttonsHTML = question.answers
-		.map((btn) => {
-			return `<button style=${
-				question.subtype === "color"
-					? `background-color:${btn === "Cream" ? "#EADBC8" : btn}`
-					: ""
-			}>${btn}</button>`;
-		})
-		.join("");
+  const buttonsHTML = question.answers
+    .map((btn) => {
+      return `<button style=${
+        question.subtype === "color"
+          ? `background-color:${btn === "Cream" ? "#EADBC8" : btn}`
+          : ""
+      }>${btn}</button>`;
+    })
+    .join("");
 
-	const stepSpansHTML = Array.from(
-		{ length: totalNumberOfSteps },
-		(_, index) => {
-			return `<span class=${currentStep >= index ? "active" : ""}></span>`;
-		}
-	).join("");
+  const stepSpansHTML = Array.from({ length: stepIndex }, (_, index) => {
+    return `<span class=${currentStep >= index ? "active" : ""}></span>`;
+  }).join("");
 
-	content.innerHTML = `
+  content.innerHTML = `
     <div class="question">
       <p style=${!question.subtitle ? "display:none" : ""}>${
-		question.subtitle
-	}</p>
+    question.subtitle
+  }</p>
       <h1>${question.title}</h1>
       <div data-button=${question.subtype} class="step-buttons">
         ${buttonsHTML}
@@ -134,63 +131,61 @@ export const showQuestion = async (currentStep) => {
     </div>
   `;
 
-	const answerButtons = document.querySelectorAll(
-		`.step-buttons[data-button="${question.subtype}"] button`
-	);
+  const answerButtons = document.querySelectorAll(
+    `.step-buttons[data-button="${question.subtype}"] button`
+  );
 
-	const prevButton = document.querySelectorAll(
-		`.prev-next-button-wrapper[data-button="${question.subtype}"] button`
-	)[0];
+  const prevButton = document.querySelectorAll(
+    `.prev-next-button-wrapper[data-button="${question.subtype}"] button`
+  )[0];
 
-	const nextButton = document.querySelectorAll(
-		`.prev-next-button-wrapper[data-button="${question.subtype}"] button`
-	)[1];
+  const nextButton = document.querySelectorAll(
+    `.prev-next-button-wrapper[data-button="${question.subtype}"] button`
+  )[1];
 
-	let selectedAnswer;
+  let selectedAnswer;
 
-	answerButtons.forEach((button, index) => {
-		button.addEventListener("click", (event) => {
-			event.preventDefault();
+  answerButtons.forEach((button, index) => {
+    button.addEventListener("click", (event) => {
+      event.preventDefault();
 
-			for (let i = 0; i < answerButtons.length; i++) {
-				if (index !== i) {
-					answerButtons[i].classList.remove("active");
-				}
-			}
+      for (let i = 0; i < answerButtons.length; i++) {
+        if (index !== i) {
+          answerButtons[i].classList.remove("active");
+        }
+      }
 
-			button.classList.add("active");
-			selectedAnswer = button.innerHTML;
-		});
-	});
+      button.classList.add("active");
+      selectedAnswer = button.innerHTML;
+    });
+  });
 
-	prevButton.addEventListener("click", (event) => {
-		event.preventDefault();
+  prevButton.addEventListener("click", (event) => {
+    event.preventDefault();
 
-		if (getStorage().step > 0) {
-			updateStorage(currentStep - 1, question.subtype, selectedAnswer);
-		}
-	});
+    if (getStorage().step > 0) {
+      updateStorage(currentStep - 1, question.subtype, selectedAnswer);
+    }
+  });
 
-	nextButton.addEventListener("click", (event) => {
-		event.preventDefault();
+  nextButton.addEventListener("click", (event) => {
+    event.preventDefault();
 
-		if (!selectedAnswer) {
-			alert("Please select an option.");
-		} else {
-			updateStorage(currentStep + 1, question.subtype, selectedAnswer);
-		}
-	});
+    if (!selectedAnswer) {
+      alert("Please select an option.");
+    } else {
+      updateStorage(currentStep + 1, question.subtype, selectedAnswer);
+    }
+  });
 };
 
 document.addEventListener("DOMContentLoaded", async () => {
-	const store = getStorage();
+  const store = getStorage();
 
-	if (!store) {
-		showQuestion(0);
-	} else {
-		const totalNumberOfSteps = await stepCounter(store);
-		store.step === totalNumberOfSteps
-			? getProducts()
-			: showQuestion(store.step);
-	}
+  if (!store) {
+    showQuestion(0);
+  } else {
+    const stepIndex = await stepCounter(store);
+    store.step === stepIndex ? getProducts() : showQuestion(store.step);
+  }
 });
--- utils/localStorage.js
+++ utils/localStorage.js
@@ -2,48 +2,48 @@ import { showQuestion } from "../app/app.js";
 import { getProducts } from "../services/products.js";
 
 export const getStorage = () => {
-	const storageData = localStorage.getItem("store");
-	return storageData ? JSON.parse(storageData) : null;
+  const storageData = localStorage.getItem("store");
+  return storageData ? JSON.parse(storageData) : null;
 };
 
 export const updateStorage = async (currentStep, subtype, value) => {
-	let store = getStorage();
+  let store = getStorage();
 
-	if (!store) {
-		localStorage.setItem(
-			"store",
-			JSON.stringify({ step: currentStep, [subtype]: value })
-		);
-		return showQuestion(currentStep);
-	}
+  if (!store) {
+    localStorage.setItem(
+      "store",
+      JSON.stringify({ step: currentStep, [subtype]: value })
+    );
+    return showQuestion(currentStep);
+  }
 
-	if (currentStep < store.step) {
-		store.step = currentStep;
-		delete store[subtype];
+  if (currentStep < store.step) {
+    store.step = currentStep;
+    delete store[subtype];
 
-		localStorage.setItem("store", JSON.stringify(store));
-		return showQuestion(currentStep);
-	}
+    localStorage.setItem("store", JSON.stringify(store));
+    return showQuestion(currentStep);
+  }
 
-	store[subtype] = value;
-	store.step = currentStep;
-	localStorage.setItem("store", JSON.stringify(store));
+  store[subtype] = value;
+  store.step = currentStep;
+  localStorage.setItem("store", JSON.stringify(store));
 
-	const totalNumberOfSteps = await stepCounter(store);
-	if (currentStep === totalNumberOfSteps) return getProducts();
+  const totalNumberOfSteps = await stepCounter(store);
+  if (currentStep === totalNumberOfSteps) return getProducts();
 
-	showQuestion(currentStep);
+  showQuestion(currentStep);
 };
 
 export const stepCounter = async (store) => {
-	if (!store) return 0;
+  if (!store) return 0;
 
-	const response = await fetch("../data/questions.json");
-	const data = await response.json();
+  const response = await fetch("../data/questions.json");
+  const data = await response.json();
 
-	const { steps } = data.find((element) => element.name === store.category) || {
-		steps: [],
-	};
+  const { steps } = data.find((element) => element.name === store.category) || {
+    steps: [],
+  };
 
-	return steps.length;
+  return steps.length;
 };
